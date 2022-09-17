3a4c66b88a6b6e179040f011b9374f9028cc9b45
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2022-09-16 22:29:33 -0400

    Full initial setup script

--- setup.bat
+++ setup.bat
@@ -8,16 +8,4 @@ del %PROJNAME%-Data.launch
 ren runClient.launch %PROJNAME%-Client.launch
 ren runServer.launch %PROJNAME%-Server.launch
 ren runData.launch %PROJNAME%-Data.launch
-
-For /F "tokens=1* delims==" %%A IN (gradle.properties) DO (
-    IF "%%A"=="modid" set modid=%%B
-)
-
-cd src\main\resources
-ren modid.mixins.json %modid%.mixins.json
-cd data
-ren modid %modid%
-cd ../assets
-ren modid %modid%
-cd ../../../..
 call gradlew processResources --no-daemon
