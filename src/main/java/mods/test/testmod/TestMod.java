25a27f3903f4639581c251799a7e9b09fd99ee6e
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-08-02 19:33:23 -0700

    Update to new toolchain

--- src/main/java/mods/test/testmod/TestMod.java
+++ src/main/java/mods/test/testmod/TestMod.java
@@ -8,10 +8,11 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(ModClassRename.MODID)
 public class ModClassRename {
 
-	public static final String MODID = "modid";
-	public static final Logger LOGGER = LogManager.getLogger(MODID);
+    public static final String MODID = "modid";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
 
-	public ModClassRename() {
-	}
+    public ModClassRename() {
+        LOGGER.info("Hello World");
+    }
 
 }
